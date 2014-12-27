d3777169964840985544279f303debd2efc00ec6
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2014-12-26 20:17:04 -0500

    add function with interpolation

---
+++ getFilteredStationDataByCity_Interpolated.R
@@ -0,0 +1,10 @@
+getFilteredStationsDataByCity_Interpolated <- function(city.list, station.list, k, begin, end, distance, hourly_interval, tolerance)
+{
+  kns <- kNStations(city.list, station.list, k)
+  weatherDFs <- dlStationData(kns, begin, end)
+  combined.list <- combineWeatherDFs(weatherDFs)
+  filteredData <- filterStationData(combined.list, distance, hourly_interval, tolerance, begin, end)
+  interpolation <- interpolateData(filteredData[[4]])
+  return(interpolation) 
+}
+
