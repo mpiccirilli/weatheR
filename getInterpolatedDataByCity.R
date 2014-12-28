dc5768e5afd80eb7b0c277a8a9f790b6d89941bb
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2014-12-27 20:45:49 -0500

    update interpolation function

--- getInterpolatedDataByCity.R
+++ getInterpolatedDataByCity.R
@@ -1,4 +1,4 @@
-getFilteredStationsDataByCity_Interpolated <- function(city.list, station.list, k, begin, end, distance, hourly_interval, tolerance)
+getInterpolatedDataByCity <- function(city.list, station.list, k, begin, end, distance, hourly_interval, tolerance)
 {
   kns <- kNStations(city.list, station.list, k)
   weatherDFs <- dlStationData(kns, begin, end)
