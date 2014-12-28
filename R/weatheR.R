83c6e120d2526e9d1ed07a36e4f3aa85ab3d0311
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2014-12-28 18:34:12 -0500

    not needed right now

--- R/weatheR.R
+++
@@ -1,7 +0,0 @@
-#' This package contains a set of functions to download, transform, and plot data from NOAA ISD weather stations.
-#'
-#' @name weatheR
-#' @docType package
-#' @author Michael Piccirilli \email{mrp2181@columbia.edu}
-#' @import httr, ggmap, FNN, McSpatial, data.table, plyr
-NULL
