f0c268c9e05b2b277f0641e81b127f9afaa2952e
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2014-12-27 20:39:03 -0500

    update filtered stations function name

--- getFilteredStationsByCity.R
+++ getFilteredStationsByCity.R
@@ -1,4 +1,4 @@
-getFilteredStationsDataByCity <- function(city.list, station.list, k, begin, end, distance, hourly_interval, tolerance)
+getFilteredStationsByCity <- function(city.list, station.list, k, begin, end, distance, hourly_interval, tolerance)
 {
   kns <- kNStations(city.list, station.list, k)
   weatherDFs <- dlStationData(kns, begin, end)
