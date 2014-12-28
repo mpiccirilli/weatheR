57f323ff9ec907cb15def5e09af2b04d5654c381
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2014-12-27 22:21:41 -0500

    removing files, to be replaced with actual package files

--- getFilteredStationsByCity.R
+++
@@ -1,8 +0,0 @@
-getFilteredStationsByCity <- function(city.list, station.list, k, begin, end, distance, hourly_interval, tolerance)
-{
-  kns <- kNStations(city.list, station.list, k)
-  weatherDFs <- dlStationData(kns, begin, end)
-  combined.list <- combineWeatherDFs(weatherDFs)
-  filteredData <- filterStationData(combined.list, distance, hourly_interval, tolerance, begin, end)
-  return(filteredData) 
-}
--- getInterpolatedDataByCity.R
+++
@@ -1,9 +0,0 @@
-getInterpolatedDataByCity <- function(city.list, station.list, k, begin, end, distance, hourly_interval, tolerance)
-{
-  kns <- kNStations(city.list, station.list, k)
-  weatherDFs <- dlStationData(kns, begin, end)
-  combined.list <- combineWeatherDFs(weatherDFs)
-  filteredData <- filterStationData(combined.list, distance, hourly_interval, tolerance, begin, end)
-  interpolation <- interpolateData(filteredData$station_data)
-  return(interpolation) 
-}
