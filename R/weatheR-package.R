f7df26a02bceac74ca001354aebcec32ca76448c
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2015-01-17 17:08:45 -0500

    add weather-package.R and manual

---
+++ R/weatheR-package.R
@@ -0,0 +1,16 @@
+#' Download, Transform, and Plot NOAA ISD weather data
+#'
+#' This is an R package with all the necessary functions to download,
+#' transform, and plot NOAA weather station data.
+#'
+#' @seealso \code{\link{allStations}}, \code{\link{getFilteredStationsByCity}},
+#' \code{\link{getInterpolatedDataByCity}}, \code{\link{getStationByID}},
+#' \code{\link{getStationsByCity}}, \code{\link{kNStations}},
+#' \code{\link{plotDailyMax}}, \code{\link{plotStations}}
+#'
+#' @name weatheR-package
+#' @aliases weatheR
+#' @docType package
+#' @author Michael Piccirilli \email{mrp2181@@columbia.edu}
+#' @import httr ggmap grid
+NULL
