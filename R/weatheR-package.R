4f466e3ab5abe47259cd41a2c00dcfab1f47451a
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2015-06-18 14:36:53 -0700

    Update weatheR-package.R

--- R/weatheR-package.R
+++ R/weatheR-package.R
@@ -11,6 +11,8 @@
 #' @name weatheR-package
 #' @aliases weatheR
 #' @docType package
-#' @author Michael Piccirilli \email{mrp2181@@columbia.edu}
+#' @author Michael Piccirilli \email{michael.r.piccirilli@@gmail.com},
+#' Elliot Cohen \email{ec2977@columbia.edu},
+#' James McCreight \email{mccreigh@gmail.com}
 #' @import httr ggmap ggplot2 grid
 NULL
